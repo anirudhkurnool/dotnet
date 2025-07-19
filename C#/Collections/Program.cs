617de47dd93720821a34c42678e06303be60a1e5
anirudhkurnool <anirudhkurnool@gmail.com>
2025-07-19 15:31:56 +0530

    collections and LINQ done.

---
+++ C#/Collections/Program.cs
@@ -0,0 +1,68 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //Arrays
+        var arr = new int[] { 1, 2, 3, 4, 5 };
+
+        foreach (int num in arr)
+        {
+            Console.WriteLine(num);
+        }
+
+        Console.WriteLine("------------------------");
+
+        Console.WriteLine($"arr.Length = {arr.Length}");
+
+        //create a new array with a extra element
+
+        arr = [.. arr, 6];
+
+
+
+
+        //var = local variable type inference
+        //var will do type inference for you
+        var lst1 = new List<int> { 1, 2, 3, 4, 5 };
+        foreach (var num in lst1)
+        {
+            Console.WriteLine(num);
+        }
+
+        //Or
+
+        Console.WriteLine("------------------------");
+
+        List<int> lst2 = new List<int> { 2, 4, 6, 8, 10 };
+        foreach (int num in lst2)
+        {
+            Console.WriteLine(num);
+        }
+
+        //OR
+        List<int> lst3 = [3, 6, 9, 12, 15];
+
+        //List methods
+        //lists are mutable
+        lst3.Add(18);//append
+        Console.WriteLine(lst3);
+
+        Console.WriteLine($"lst3 capacity = {lst3.Capacity}");
+        Console.WriteLine("");
+        //0-indexed
+        Console.WriteLine($"lst3[0] = {lst3[0]}");
+        Console.WriteLine($"Lenght of lst3 = {lst3.Count}");
+        Console.WriteLine($"lst3[^1] = lst3[lst3.Count - 1] = {lst3[^1]}");
+
+        foreach (int num in lst3[2..4])//list slicing - [2, 4)
+        {
+            Console.WriteLine(num);
+        }
+
+        //list sorting
+        List<int> lst4 = [3, 41, 13, 4, 414, 423414, 1212];
+        Console.WriteLine($"Index of 4 in lst4 = {lst4.IndexOf(4)}");
+        lst4.Sort(); //inplace
+        Console.WriteLine($"Index of 4 in lst4 = {lst4.IndexOf(4)}");
+    }
+}
---
+++ C#/LINQ/Program.cs
@@ -0,0 +1,72 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //LINQ = Language Integrated Query
+        //LINQ makes working with collections much simpler
+        //LINQ is a type of declarative programming like SQL
+
+        List<int> scores = [93, 12, 42, 64, 23, 46, 77, 87, 76, 90];
+
+        for (int i = 0; i < scores.Count; i++)
+        {
+            if (scores[i] > 80)
+            {
+                Console.WriteLine($"Found a score greater than 80 : {scores[i]}");
+            }
+        }
+
+        //Equivalent of the above code in LINQ
+        //differed execution
+        IEnumerable<int> scoresGreaterThan80 = from score in scores where score > 80 select score;
+        foreach (int score in scoresGreaterThan80)
+        {
+            Console.WriteLine($"Found a score greater than 80 : {score}");
+        }
+
+        IEnumerable<int> scoresGreaterThan80SortedDesc = from score in scores where score > 80 orderby score descending select score;
+        foreach (int score in scoresGreaterThan80SortedDesc)
+        {
+            Console.Write($"{score} ");
+        }
+        Console.WriteLine("");
+
+        IEnumerable<int> scoresGreaterThan80SortedAsc = from score in scores where score > 80 orderby score ascending select score;
+        foreach (int score in scoresGreaterThan80SortedAsc)
+        {
+            Console.Write($"{score} ");
+        }
+        Console.WriteLine("");
+
+        IEnumerable<string> scoresGreaterThan80SortedAscString = from score in scores where score > 80 orderby score ascending select $"Score : {score}";
+
+        Console.WriteLine($"Number of scores = {scoresGreaterThan80SortedAscString.Count()}");
+
+        foreach (string scoreString in scoresGreaterThan80SortedAscString)
+        {
+            Console.WriteLine(scoreString);
+        }
+
+        //to convert IEnumerable<T> to List<T>
+        List<string> scoresGreaterThan80SortedAscListofStrings = scoresGreaterThan80SortedAscString.ToList();
+        //or
+        List<string> scoresGreaterThan80SortedAscListofStringsCopy = [.. scoresGreaterThan80SortedAscString];
+
+        //LINQ compiles down to a sequnece of method calls on the object
+        //the above query actually looks like 
+        //the thing inside () beside Where is called as lambda function
+        //the below thing is called as method query
+        IEnumerable<int> scoreMethodQuery = scores.Where(score => score > 80);
+        foreach (int score in scoreMethodQuery)
+        {
+            Console.WriteLine($"Found a score greater than 80 : {score}");
+        }
+
+        IEnumerable<int> scoreMethodQueryDesc = scores.Where(score => score > 80).OrderByDescending(score => score);
+        foreach (int score in scoreMethodQuery)
+        {
+            Console.WriteLine($"Found a score greater than 80 : {score}");
+        }
+       
+    }
+}
